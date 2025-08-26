f47fca4d076ec270a68992f1156ccde15603cb55
郑雨昊 <1067992289@qq.com>
2025-08-26 14:34:56 +0800

    Create cri-docker-config.sh

---
+++ EdgeLLM/Docker/cri-docker-config.sh
@@ -0,0 +1,44 @@
+cat > /etc/systemd/system/cri-dockerd.service<<-EOF
+[Unit]
+Description=CRI Interface for Docker Application Container Engine
+Documentation=https://docs.mirantis.com
+After=network-online.target firewalld.service docker.service
+Wants=network-online.target
+Requires=cri-docker.socket
+ 
+[Service]
+Type=notify
+ExecStart=/usr/local/bin/cri-dockerd --pod-infra-container-image=registry.cn-hangzhou.aliyuncs.com/google_containers/pause:3.10
+ --network-plugin=cni --cni-conf-dir=/etc/cni/net.d --cni-bin-dir=/opt/cni/bin --container-runtime-endpoint=unix:///var/run/cri-dockerd.sock --cri-dockerd-root-directory=/var/lib/dockershim --docker-endpoint=unix:///var/run/docker.sock --cri-dockerd-root-directory=/var/lib/docker
+ExecReload=/bin/kill -s HUP $MAINPID
+TimeoutSec=0
+RestartSec=2
+Restart=always
+StartLimitBurst=3
+StartLimitInterval=60s
+LimitNOFILE=infinity
+LimitNPROC=infinity
+LimitCORE=infinity
+TasksMax=infinity
+Delegate=yes
+KillMode=process
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+
+cat > /etc/systemd/system/cri-docker.socket <<-EOF
+[Unit]
+Description=CRI Docker Socket for the API
+PartOf=cri-docker.service
+ 
+[Socket]
+ListenStream=/var/run/cri-dockerd.sock
+SocketMode=0660
+SocketUser=root
+SocketGroup=docker
+ 
+[Install]
+WantedBy=sockets.target
+EOF
